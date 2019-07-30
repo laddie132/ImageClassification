fd0ef06a97fef28a7bd9dbd923e21b8f2d5336c9
Han <liuhan132@foxmail.com>
2019-07-30 16:54:50 +0800

    Fix with the new model

--- train.sh
+++ train.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
 
-SUB_PATH=weed-inception-inception_resnet
+SUB_PATH=weed-inaturalist-inception-inception_resnet
 IMAGE_LIST_NAME=weed_image_lists_mix_oversample.json
 IMAGE_PATH_NAME=weed_photos_resize_mix
-TFHUB_MODULE="hub_modules/inception-v3-feature"\ "hub_modules/inception_resnet_v2"
+TFHUB_MODULE="hub_modules/inaturalist-inception_v3"\ "hub_modules/inception_resnet_v2"
 
 CUDA_VISIBLE_DEVICES=0 python retrain.py \
     --image_dir /home/lh/$IMAGE_PATH_NAME \
@@ -12,6 +12,6 @@ CUDA_VISIBLE_DEVICES=0 python retrain.py \
     --tfhub_module $TFHUB_MODULE \
     --module_trainable 0
 
-python rebuild_model.py \
+CUDA_VISIBLE_DEVICES=0 python rebuild_model.py \
     --model_dir outputs/${SUB_PATH}/saved_model_b64 \
     --origin_model_dir outputs/${SUB_PATH}/saved_model
